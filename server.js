d9dd463e9745fe29f94528b3c5636c611ee090e8
mahdyardigry <29mah12@gmail.com>
2026-07-29 10:43:46 +0330

    Refactor server.js to handle coin data filtering

--- server.js
+++ server.js
@@ -1,118 +1,54 @@
-const express = require("express");
-const path = require("path");
 
-const app = express();
-
-app.use(express.static(__dirname));
-
-let cache = [];
-let lastUpdate = 0;
-
-
-// صفحه اصلی
-app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname,"index.html"));
-});
+const stable = [
+"USDT",
+"USDC",
+"DAI",
+"USDS",
+"USD1",
+"USDE",
+"USDD",
+"RLUSD",
+"PYUSD"
+];
 
 
-// دریافت پامپ ها
-app.get("/api/pumps", async(req,res)=>{
+const coins = data
 
-    try {
+.filter(c => 
+ !stable.includes(c.symbol.toUpperCase())
+)
 
-        // هر 60 ثانیه فقط یک بار از CoinGecko بگیر
-        if(cache.length > 0 && Date.now() - lastUpdate < 60000){
-            return res.json(cache);
-        }
+.filter(c =>
+ Number(c.price_change_percentage_24h) > 3
+)
 
+.map(c=>({
 
-        const url =
-        "https://api.coingecko.com/api/v3/coins/markets"+
-        "?vs_currency=usd"+
-        "&order=percent_change_24h_desc"+
-        "&per_page=50"+
-        "&page=1"+
-        "&sparkline=false";
+symbol:c.symbol.toUpperCase(),
 
+name:c.name,
 
-        const response = await fetch(url,{
-            headers:{
-                "accept":"application/json",
-                "user-agent":"PumpScanner"
-            }
-        });
+price:c.current_price,
 
+change24h:
+Number(c.price_change_percentage_24h || 0)
+.toFixed(2),
 
-        if(!response.ok){
-            throw new Error(
-                "CoinGecko status "+response.status
-            );
-        }
+volume:c.total_volume,
 
+marketcap:c.market_cap,
 
-        const data = await response.json();
+volumeRatio:
+((c.total_volume / c.market_cap)*100)
+.toFixed(2),
+
+image:c.image
+
+}))
 
 
-        const coins = data.map(c=>({
+.sort((a,b)=>
+Number(b.change24h)-Number(a.change24h)
+)
 
-            symbol:c.symbol.toUpperCase(),
-
-            name:c.name,
-
-            price:c.current_price,
-
-            change24h:
-            Number(c.price_change_percentage_24h || 0)
-            .toFixed(2),
-
-            volume:c.total_volume,
-
-            marketcap:c.market_cap,
-
-            image:c.image
-
-        }));
-
-
-        cache = coins;
-
-        lastUpdate = Date.now();
-
-
-        res.json(coins);
-
-
-    }catch(error){
-
-
-        console.log(error.message);
-
-
-        // اگر API خراب شد اطلاعات قبلی را بده
-        if(cache.length){
-            return res.json(cache);
-        }
-
-
-        res.status(500).json({
-
-            error:"Data provider unavailable"
-
-        });
-
-    }
-
-});
-
-
-
-const PORT = process.env.PORT || 8080;
-
-
-app.listen(PORT,()=>{
-
-console.log(
-"Pump Scanner running on "+PORT
-);
-
-});
+.slice(0,20);
