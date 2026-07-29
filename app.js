6d146222cabe6191a2ccc8a9a3869203178717f9
mahdyardigry <29mah12@gmail.com>
2026-07-29 08:38:06 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
 const coins = document.getElementById("coins");
+const whale = document.getElementById("whale");
+const smart = document.getElementById("smart");
 
-coins.innerHTML="⏳ دریافت اطلاعات...";
+coins.innerHTML = "⏳ درحال دریافت اطلاعات...";
 
 fetch("/api/pumps")
-.then(res=>res.json())
-.then(data=>{
+.then(res => res.json())
+.then(data => {
 
-coins.innerHTML="";
+    coins.innerHTML = "";
 
-data.forEach(c=>{
+    data.forEach(c => {
 
-coins.innerHTML += `
-<div style="padding:10px;border-bottom:1px solid #444">
-<b>${c.symbol}</b><br>
-🚀 رشد: ${Number(c.priceChangePercent).toFixed(2)}%
-<br>
-💰 حجم: ${Number(c.quoteVolume).toLocaleString()}
-</div>
-`;
+        coins.innerHTML += `
+        <div style="
+        padding:10px;
+        border-bottom:1px solid #333;
+        ">
+        <b>${c.symbol}</b><br>
 
-});
+        🚀 تغییر:
+        ${parseFloat(c.priceChangePercent).toFixed(2)} %
+
+        <br>
+
+        💰 قیمت:
+        ${parseFloat(c.lastPrice).toFixed(6)}
+
+        <br>
+
+        📊 حجم:
+        ${Number(c.quoteVolume).toLocaleString()}
+
+        </div>
+        `;
+
+    });
 
 })
-.catch(err=>{
+.catch(error => {
 
-coins.innerHTML="❌ خطا در دریافت اطلاعات";
+    console.log(error);
+
+    coins.innerHTML =
+    "❌ خطا در دریافت اطلاعات";
 
 });
+
+
+whale.innerHTML =
+"🐋 Whale Scanner آماده شد";
+
+
+smart.innerHTML =
+"🧠 Smart Money در نسخه بعدی اضافه می‌شود";
