b42f840e916e29f78406c8c35b793bc5ef99ae97
mahdyardigry <29mah12@gmail.com>
2026-07-29 08:25:53 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const path = require("path");
 
@@ -10,31 +9,38 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Binance data proxy
 app.get("/api/pumps", async (req, res) => {
   try {
-    const response = await fetch("https://api.binance.com/api/v3/ticker/24hr");
+    const response = await fetch(
+      "https://api.binance.com/api/v3/ticker/24hr"
+    );
+
     const data = await response.json();
 
     const list = data
       .filter(c => c.symbol.endsWith("USDT"))
-      .sort((a, b) =>
-        parseFloat(b.priceChangePercent) -
-        parseFloat(a.priceChangePercent)
+      .sort(
+        (a, b) =>
+          parseFloat(b.priceChangePercent) -
+          parseFloat(a.priceChangePercent)
       )
       .slice(0, 20);
 
     res.json(list);
 
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       error: "Binance API failed"
     });
   }
 });
 
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log("Pump Scanner started on port " + PORT);
+  console.log(
+    "Pump Scanner started on port " + PORT
+  );
 });
